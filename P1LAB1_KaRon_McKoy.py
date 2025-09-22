14c147ad33b723b59c90a8df2fd721f8ad4c1c26
mckoykftcc <mckoyk9966@student.faytechcc.edu>
2025-09-21 23:42:37 -0400

    Add files via upload

---
+++ P1LAB1_KaRon_McKoy.py
@@ -0,0 +1,13 @@
+# KaRon McKoy
+# 2025-09-21
+# P1LAB1 – Welcome Message
+# This program prompts the user to enter their first and last name separately,
+# then displays a personalized welcome message using their input.
+# Prompt user to enter their first name
+first_name = input("Enter your first name: ")
+
+# Prompt user to enter their last name
+last_name = input("Enter your last name: ")
+
+# Print a welcome message
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110.")
